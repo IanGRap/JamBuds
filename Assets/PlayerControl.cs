3ec9cd868d59b9d02321cc943c1bc3895d86af35
jwynd <18224844+jwynd@users.noreply.github.com>
2018-11-07 18:23:08 -0800

    created grid and placeholder player with constant rightward motion

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    private int speed = 5;
+	// Use this for initialization
+    public Transform transform;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+	}
+}
