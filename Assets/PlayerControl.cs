661823cc7e3fa86792cea4791c805263bcd4437e
jwynd <18224844+jwynd@users.noreply.github.com>
2018-11-14 18:15:20 -0800

    added ability to continue playing old sounds, can have exactly 2. Hardcoded, will require modification

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
+
+    // We are going to start out by hardcoding the ability for two animals to play sounds while
+    // we take the time to learn how to do this more dynamically
+
     public float speed = 5.0f;    // this controls the speed of the constant rightward motion
     public GameObject player;// This is a reference to the player, remember to drag and drop player object
     public Material noteMaterial; // this is a public variable containing the note materail
     public bool useSpaces = true; // if true, the notes are placed in the middle of the spaces, not on the lines
     private GameObject[] Notes = new GameObject[8]; // these will keep references to the note objects
-//    private int numNotes = 0; // this keeps track of how many notes have been placed
     private bool[] isInCol = new bool[] {false, false, false, false, false, false, false, false}; // this keeps track of the collumns which already have notes
-    public AudioClip topSound;      // the track associated with the top row
-    public AudioClip middleSound;    // The track associated with the middle row
-    public AudioClip bottomSound;    // the track associated with the bottom sound
-    private AudioSource aS;        // Audio Source reference, used to play, pause, and manage the audio
+    private float[] prevNotePosition = new float[8]; // this will hold the x positions of previous notes so that they can be replayed later
+    private bool[] wasUsed = new bool[] {false, false, false, false, false, false, false, false};
+    private bool isComplete = false; // this will become true when the player is placing new sounds for the second pass
+    public AudioClip[] sounds = new AudioClip[6]; // array to hold the audio clips that will be used
+    private AudioSource[] aS;        // Audio Source reference, used to play, pause, and manage the audio
+
+    // these hold the indeces of sounnds with locations of current top middle and bottom
+    private int top = 0;
+    private int middle = 1;
+    private int bottom = 2;
 
     ///////////////////////////////
     //LOOK BELOW TO MODIFY VALUES//
@@ -23,8 +32,9 @@ public class PlayerControl : MonoBehaviour {
     private const float staffEdge = 18.0f;          // location of the edge of the staff
     private float[] colEdge = new float[] {-13.5f, -9.0f, -4.5f, 0.0f, 4.5f, 9.0f, 13.5f, 18.0f}; // location where notes can be placed
     private float[] noteOffset = new float[] {0.3f, 0.2f, 0.1f, 0.0f, -0.1f, -0.2f, -0.3f, -0.4f}; // these offset the position of the notes to account for paralax
+
 	void Start () {
-        aS = gameObject.GetComponent<AudioSource>();
+        aS = gameObject.GetComponents<AudioSource>();
 
         // move the position of the notes to the center of the spaces if option is selected
         if(useSpaces){
@@ -96,23 +106,57 @@ public class PlayerControl : MonoBehaviour {
         for(int i = 0; i < 8; i++){
             if(player.transform.position.x > colEdge[i]+noteOffset[i]-0.05f && player.transform.position.x < colEdge[i]+noteOffset[i]+0.05f && isInCol[i]){
                 if(Notes[i].transform.position.z > 1.0f){
-                    addAndPlay(topSound);
+                    addAndPlay(sounds[top], 0);
                 }
                 else if(Notes[i].transform.position.z < -1.0f){
-                    addAndPlay(bottomSound);
+                    addAndPlay(sounds[bottom], 0);
                 }
                 else{
-                    addAndPlay(middleSound);
+                    addAndPlay(sounds[middle], 0);
                 }
             }
         }
-	}
 
+        if(Input.GetKeyDown(KeyCode.N) && !isComplete){
+            for(int i = 0; i < Notes.Length; i++){
+                if(isInCol[i]){
+                    prevNotePosition[i] = Notes[i].transform.position.z;
+                }
+                wasUsed[i] = isInCol[i];
+                isInCol[i] = false;
+                Destroy(Notes[i]);
+            }
+            top += 3;
+            middle += 3;
+            bottom += 3;
+            isComplete = true;
+        }
 
-    public void addAndPlay(AudioClip inputClip){
-        aS.clip = inputClip;
-        if(aS.clip != null){
-            aS.Play();
+        if(isComplete){
+            playBackground();
+        }
+    }
+
+    public void playBackground(){
+        for(int i = 0; i < 8; i++){
+            if(player.transform.position.x > colEdge[i]+noteOffset[i]-0.05f && player.transform.position.x < colEdge[i]+noteOffset[i]+0.05f && wasUsed[i]){
+                if(prevNotePosition[i] > 1.0f){
+                    addAndPlay(sounds[0], 1);
+                }
+                else if(prevNotePosition[i] < -1.0f){
+                    addAndPlay(sounds[2], 1);
+                }
+                else{
+                    addAndPlay(sounds[1], 1);
+                }
+            }
+        }
+    }
+
+    public void addAndPlay(AudioClip inputClip, int index){
+        aS[index].clip = inputClip;
+        if(aS[index].clip != null){
+            aS[index].Play();
         }
         else {
             print("no audio clip provided");
